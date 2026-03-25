7ad68fa3908035e51122d9b5e5f5f20fe4099e05
matteo lagona <lusdaassociation@gmail.com>
2026-03-25 09:34:06 +0100

    Fix build: Update Astro output mode to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  output: "hybrid",
+  output: "static",
   adapter: cloudflare({
     platformProxy: {
       enabled: true
